e3a4a4e605b64ba6f521f33c4ad1caa00a4ea422
Johannson17 <116839002+Johannson17@users.noreply.github.com>
2023-06-21 00:53:19 -0300

    Update settings.py

--- Mysite/settings.py
+++ Mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'RENDER' not in os.environ
-DEBUG = FALSE
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
